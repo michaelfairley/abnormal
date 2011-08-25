b7ec1f3bb8e41a73a63a2cd2a0e81390930dc997
Michael Fairley <michaelfairley@gmail.com>
2011-08-25 00:16:04 -0700

    Start a new test

--- lib/abnormal.rb
+++ lib/abnormal.rb
@@ -1,3 +1,16 @@
-class Abnormal
+require 'abnormal/version'
 
+class Abnormal
+  def self.db; @@db; end
+  def self.db=(db)
+    @@db = db
+  end
+
+  def self.ab_test(identity, test_name, alternatives, conversions)
+    db['tests'].update({:name => test_name}, {:$set => {:alternatives => alternatives, :id => Digest::MD5.hexdigest(test_name)}}, :upsert => true)
+  end
+
+  def self.get_test(test_id)
+    db['tests'].find_one(:id => test_id)
+  end
 end
--- spec/abnormal_spec.rb
+++ spec/abnormal_spec.rb
@@ -7,10 +7,16 @@ require 'abnormal'
 Bundler.require(:development)
 
 describe Abnormal do
+  before(:all) do
+    Abnormal.db = Mongo::Connection.new['abnormal_test']
+    Abnormal.db['tests'].drop
+  end
+
   describe 'ab_test' do
     describe 'the first call for a new test' do
       it 'adds that test to the test table' do
-
+        Abnormal.ab_test('id', 'test', [1, 2], 'conversion')
+	Abnormal.get_test(Digest::MD5.hexdigest('test'))['name'].should == 'test'
       end
     end
   end
