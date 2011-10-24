dbfd57edeb08fe71134fa88c5c6c08c8d4806c6f
Michael Fairley <michaelfairley@gmail.com>
2011-10-24 00:37:01 -0700

    Doh. The travis script needs a shebang

--- ci/travis.rb
+++ ci/travis.rb
@@ -1,8 +1,10 @@
+#!/usr/bin/env ruby
+
 if ENV['TESTX']
-  system "
+  system <<-CMD
     cd test/#{ENV['TESTX']}
     BUNDLE_GEMFILE=Gemfile bundle exec rake test
-  "
+  CMD
 else
   system 'rake'
 end
